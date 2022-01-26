9de1ff2dfc67f808f4173d03fd7c2317676567ea
LouisTsiattalou <louis.tsiattalou@cma.gov.uk>
2022-01-26 14:12:45 +0000

    Add Stage 3 and Stage 4 template deployment commands

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ BUCKET_NAME="" # Bucket Name to deploy to
 SOLUTION_NAME="" # Application Name
 VERSION="" # Version being deployed
 ROOT="" # Root of Repository
+HOSTEDZONEID="" # Hosted Zone from Route53 : eg mycompanydomain.com
+RECORDNAME="" # Record Set Name for domain : eg sftpapi.mycompanydomain.com
 
 cd $ROOT/deployment
 bash build-s3-dist.sh $BUCKET_NAME $SOLUTION_NAME $VERSION
@@ -15,3 +17,7 @@ cd global-s3-assets/
 
 aws cloudformation deploy --template-file 01-sftp-vpc.template --stack-name sftp-vpc-stack --capabilities CAPABILITY_IAM
 aws cloudformation deploy --template-file 02-sftp-cognito.template --stack-name sftp-cognito-stack --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file 03-sftp-endpoint.template --stack-name sftp-endpoint-stack --capabilities CAPABILITY_IAM
+
+# TODO Add HostedZoneID and RecordName to in 04-sftp-ecs.template as params passed in
+aws cloudformation deploy --template-file 04-sftp-ecs.template --stack-name sftp-ecs-stack --capabilities CAPABILITY_IAM --parameter-overrides HostedZoneId=$HOSTEDZONEID RecordName=$RECORDNAME

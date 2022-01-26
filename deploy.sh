93c044d09c9e708355648f9618a9cc27dab29db2
LouisTsiattalou <louis.tsiattalou@cma.gov.uk>
2022-01-26 14:19:12 +0000

    Cleanup TODO

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,11 @@ bash build-s3-dist.sh $BUCKET_NAME $SOLUTION_NAME $VERSION
 aws s3 sync regional-s3-assets/ s3://${BUCKET_NAME}/${SOLUTION_NAME}/${VERSION}/
 cd global-s3-assets/
 
-# TODO - Megatemplate that defines the other templates in a nested stack. Will also need to sync global-s3-assets for that.
+# TODO - Megatemplate that defines the other templates in a nested stack. Will
+# also need to sync global-s3-assets for that, with all these templates below.
+# Will greatly simplify deployment assuming all prerequisites are met.
 
 aws cloudformation deploy --template-file 01-sftp-vpc.template --stack-name sftp-vpc-stack --capabilities CAPABILITY_IAM
 aws cloudformation deploy --template-file 02-sftp-cognito.template --stack-name sftp-cognito-stack --capabilities CAPABILITY_IAM
 aws cloudformation deploy --template-file 03-sftp-endpoint.template --stack-name sftp-endpoint-stack --capabilities CAPABILITY_IAM
-
-# TODO Add HostedZoneID and RecordName to in 04-sftp-ecs.template as params passed in
 aws cloudformation deploy --template-file 04-sftp-ecs.template --stack-name sftp-ecs-stack --capabilities CAPABILITY_IAM --parameter-overrides HostedZoneId=$HOSTEDZONEID RecordName=$RECORDNAME

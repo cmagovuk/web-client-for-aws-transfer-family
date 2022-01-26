098363dfbccbdd9e86d11d48f52286db17aba49f
LouisTsiattalou <louis.tsiattalou@cma.gov.uk>
2022-01-26 13:52:45 +0000

    Add cfn deploy step for cognito

--- deploy.sh
+++ deploy.sh
@@ -14,3 +14,4 @@ cd global-s3-assets/
 # TODO - Megatemplate that defines the other templates in a nested stack. Will also need to sync global-s3-assets for that.
 
 aws cloudformation deploy --template-file 01-sftp-vpc.template --stack-name sftp-vpc-stack --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file 02-sftp-cognito.template --stack-name sftp-cognito-stack --capabilities CAPABILITY_IAM
